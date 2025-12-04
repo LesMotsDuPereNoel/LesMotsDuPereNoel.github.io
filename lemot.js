805765526d950f742c46eafaf238534a902c41dc
Christopher <adam2@example.com>
2025-12-04 13:03:52 -0500

    les changements

---
+++ lemot.js
@@ -0,0 +1,80 @@
+let choix = 5//extraire le vrai choix de l'html mais suppose que c'est 6
+if (choix == 5){
+    let choixBanqueMot5 = Math.floor(Math.random() * 10)
+    let motReponse = ""
+    switch (choixBanqueMot5){
+        case 0:
+            motReponse = "buche"
+            break;
+        case 1:
+            motReponse = "hiver"
+            break;
+        case 2:
+            motReponse = "dinde"
+            break;
+        case 3:
+            motReponse = "jouet"
+            break;
+        case 4:
+            motReponse = "sapin"
+            break;
+        case 5:
+            motReponse = "avent"
+            break;
+        case 6:
+            motReponse = "lutin"
+            break;
+        case 7:
+            motReponse = "botte"
+            break;
+        case 8:
+            motReponse = "froid"
+            break;
+        case 9:
+            motReponse = "givre"
+            break;
+        case 10:
+            motReponse = "glace"
+            break;
+    }
+}
+
+if (choix == 6){
+    let choixBanqueMot5 = Math.floor(Math.random() * 10)
+    let motReponse = ""
+    switch (choixBanqueMot5){
+        case 0:
+            motReponse = "buche"
+            break;
+        case 1:
+            motReponse = "hiver"
+            break;
+        case 2:
+            motReponse = "dinde"
+            break;
+        case 3:
+            motReponse = "jouet"
+            break;
+        case 4:
+            motReponse = "sapin"
+            break;
+        case 5:
+            motReponse = "avent"
+            break;
+        case 6:
+            motReponse = "lutin"
+            break;
+        case 7:
+            motReponse = "botte"
+            break;
+        case 8:
+            motReponse = "froid"
+            break;
+        case 9:
+            motReponse = "givre"
+            break;
+        case 10:
+            motReponse = "glace"
+            break;
+    }
+}
